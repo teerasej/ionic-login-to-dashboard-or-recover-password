a3aee28dc6acf6cbe9837da1e0a9aa7b04dfc666
Teerasej <teerasej@gmail.com>
2019-06-13 13:38:47 +0700

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  message = "...";
+
+  // NavController
+  constructor(private nav: NavController) {
+
+  }
+
+  signIn(username, password) {
+    console.log('OK', username, password);
+
+
+    if (username === 'admin' && password === '1234') {
+      this.nav.navigateForward('/dashboard');
+    } else {
+      this.message = "Please check username and password, try again";
+    }
+
+    alert(this.message);
+  }
+
+  recoverPassword() {
+    console.log('Recovering password...');
+  }
 
 }
